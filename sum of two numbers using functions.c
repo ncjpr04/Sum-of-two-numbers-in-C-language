f6779c819548702e2d24f4e05962f260d0512b24
Nitin Choudhary <116860169+ncjpr04@users.noreply.github.com>
2023-08-10 12:17:55 +0530

    Add files via upload

---
+++ sum of two numbers using functions.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int sum(int a,int b);
+int main()
+{
+  int a,b;
+  printf("enter first number: ");
+  scanf("%d",&a);
+  printf("enter second number: ");
+  scanf("%d",&b);
+  int s = sum(a,b);
+printf("sum is : %d\n",s);
+return 0;
+}
+int sum(int x, int y) {
+  return x+y;
+}
